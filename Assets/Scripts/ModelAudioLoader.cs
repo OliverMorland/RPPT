1edf0d654c4cc378afd9b8ba671081e805a6ffda
Oliver Morland <oliversimonedward@hotmail.co.uk>
2023-03-25 13:22:46 -0400

    Latest changes

--- Assets/Scripts/ModelAudioLoader.cs
+++ Assets/Scripts/ModelAudioLoader.cs
@@ -6,6 +6,10 @@ using UnityEngine.Events;
 using UnityEngine.Networking;
 using TMPro;
 using UnityEngine.UI;
+using System.Diagnostics;
+using System.ComponentModel;
+using Debug = UnityEngine.Debug;
+using AnotherFileBrowser.Windows;
 
 public class ModelAudioLoader : MonoBehaviour
 {
@@ -29,8 +33,16 @@ public class ModelAudioLoader : MonoBehaviour
 
     public void OpenFileExplorer()
     {
-        path = EditorUtility.OpenFilePanel("Find Model Audio Track", "", "mp3");
-        StartCoroutine(LoadAudioClip(path));
+        //path = EditorUtility.OpenFilePanel("Find Model Audio Track", "", "mp3");
+        var bp = new BrowserProperties();
+        bp.filter = "Image files (*.mp3) | *.mp3";
+        bp.filterIndex = 0;
+
+        new FileBrowser().OpenFileBrowser(bp, path =>
+        {
+            //Load image from local path with UWR
+            StartCoroutine(LoadAudioClip(path));
+        });
     }
 
     IEnumerator LoadAudioClip(string path)
@@ -69,6 +81,75 @@ public class ModelAudioLoader : MonoBehaviour
     {
         return m_captionInput.text;
     }
+
+    //Sample code for opening on mac and PC
+    //public static void OpenInMacFileBrowser(string path)
+    //{
+    //    bool openInsidesOfFolder = false;
+
+    //    // try mac
+    //    string macPath = path.Replace("\\", "/"); // mac finder doesn't like backward slashes
+
+    //    if (Directory.Exists(macPath)) // if path requested is a folder, automatically open insides of that folder
+    //    {
+    //        openInsidesOfFolder = true;
+    //    }
+
+    //    //Debug.Log("macPath: " + macPath);
+    //    //Debug.Log("openInsidesOfFolder: " + openInsidesOfFolder);
+
+    //    if (!macPath.StartsWith("\""))
+    //    {
+    //        macPath = "\"" + macPath;
+    //    }
+    //    if (!macPath.EndsWith("\""))
+    //    {
+    //        macPath = macPath + "\"";
+    //    }
+    //    string arguments = (openInsidesOfFolder ? "" : "-R ") + macPath;
+    //    //Debug.Log("arguments: " + arguments);
+    //    try
+    //    {
+    //        System.Diagnostics.Process.Start("open", arguments);
+    //    }
+    //    catch (System.ComponentModel.Win32Exception e)
+    //    {
+    //        // tried to open mac finder in windows
+    //        // just silently skip error
+    //        // we currently have no platform define for the current OS we are in, so we resort to t$$anonymous$$s
+    //        e.HelpLink = ""; // do anyt$$anonymous$$ng with t$$anonymous$$s variable to silence warning about not using it
+    //    }
+    //}
+
+    //public static void OpenInWinFileBrowser(string path)
+    //{
+    //    bool openInsidesOfFolder = false;
+
+    //    // try windows
+    //    string winPath = path.Replace("/", "\\"); // windows explorer doesn't like forward slashes
+
+    //    if (Directory.Exists(winPath)) // if path requested is a folder, automatically open insides of that folder
+    //    {
+    //        openInsidesOfFolder = true;
+    //    }
+    //    try
+    //    {
+    //        System.Diagnostics.Process.Start("explorer.exe", (openInsidesOfFolder ? "/root," : "/select,") + winPath);
+    //    }
+    //    catch (System.ComponentModel.Win32Exception e)
+    //    {
+    //        // tried to open win explorer in mac
+    //        // just silently skip error
+    //        // we currently have no platform define for the current OS we are in, so we resort to t$$anonymous$$s
+    //        e.HelpLink = ""; // do anyt$$anonymous$$ng with t$$anonymous$$s variable to silence warning about not using it
+    //    }
+    //}
+
+    //public static void OpenInFileBrowser(string path)
+    //{
+    //    OpenInWinFileBrowser(path);
+    //    OpenInMacFileBrowser(path);
+    //}
 
 
 }
